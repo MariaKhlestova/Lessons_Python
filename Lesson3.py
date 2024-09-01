2600960bd28e2937e51b02091136a31b30385fb6
MariaKhlestova <selebina.maria@yandex.ru>
2024-09-01 20:25:00 +0300

    update

--- Lesson3.py
+++ Lesson3.py
@@ -99,6 +99,31 @@ print(s)
 # Недавно компания выпустила новую линейку видеокарт. Самые старшие поколения разобрали за пару дней.
 # Напишите программу, которая удаляет наибольшие элементы из списка видеокарт.
 
+videoCardsNumber = int(input("Введите количество видеокарт: "))
+videoCards = []
+newVideoCardsList = []
+maxItem = 0
+
+for item in range (videoCardsNumber):
+    videoCards.append(int(input(f'Видеокарта {item + 1}: ')))
+   
+    if videoCards[item] > maxItem:
+        maxItem = videoCards[item]
+        
+for item in range (videoCardsNumber):
+    if videoCards[item] != maxItem:
+        newVideoCardsList.append(videoCards[item])
+
+print()
+print("Старый список видеокарт: [", end=' ')
+for item in range (videoCardsNumber):
+    print(videoCards[item], end=' ')
+print(']')
+print("Новый список видеокарт: [", end=' ')
+for item in range (len(newVideoCardsList)):
+    print(newVideoCardsList[item], end=' ')
+print(']')
+
 # Задача 2: Кино
 # Илья зашёл на любительский киносайт, на котором пользователи оставляют рецензии на фильмы. 
 # Их список: films = [‘Крепкий орешек’, ‘Назад в будущее’, ‘Таксист’, ‘Леон’, ‘Богемская рапсодия’, ‘Город грехов’, ‘Мементо’, ‘Отступники’, ‘Деревня’]
