5217c26a224696db89fac472d8246c720e400472
MariaKhlestova <selebina.maria@yandex.ru>
2024-09-03 00:15:02 +0300

    update

--- Lesson5.py
+++ Lesson5.py
@@ -95,7 +95,7 @@ for _ in range(sites_count):
         display_struct(i_site)
 """
 # Задача 3. Продвинутая функция sum
-
+"""
 def my_sum(*args):
     total_sum = 0
     
@@ -109,5 +109,22 @@ def my_sum(*args):
 # код для тестирования
 print(my_sum([[1, 2, [3]], [1], 3]))
 print(my_sum(1, 2, 3, 4, 5))
+"""
+# Задача 4. Список списков
+
+def flatten(a_list):
+    result = []
+    
+    for e in a_list:
+        if isinstance(e, int):
+            result.append(e)
+        else:
+            result.extend(flatten(e))
+    return result
+
+nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]], [[11, 12, 13], [14, 15], [16, 17, 18]]]
+flattened_list = flatten(nice_list)
+
+print(flattened_list)
 
             
