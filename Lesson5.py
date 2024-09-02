a9de0bfb1dffe4290df178f535acc3883789a297
MariaKhlestova <selebina.maria@yandex.ru>
2024-09-03 00:09:04 +0300

    update

--- Lesson5.py
+++ Lesson5.py
@@ -41,7 +41,7 @@ while True:
     print('Значение ключа: ', find_key(struct=site, max_depth=max_depth, key=key))
 """
 # Задача 2. Глубокое копирование
-
+"""
 import copy
 
 site = {
@@ -93,6 +93,21 @@ for _ in range(sites_count):
     
     for i_site in sites:
         display_struct(i_site)
-        
+"""
+# Задача 3. Продвинутая функция sum
+
+def my_sum(*args):
+    total_sum = 0
+    
+    for i_elem in args:
+        if isinstance(i_elem, int):
+            total_sum += i_elem
+        elif isinstance(i_elem, (list, tuple)):
+            for x in i_elem:
+                total_sum += my_sum(x)
+    return total_sum
+# код для тестирования
+print(my_sum([[1, 2, [3]], [1], 3]))
+print(my_sum(1, 2, 3, 4, 5))
 
             
