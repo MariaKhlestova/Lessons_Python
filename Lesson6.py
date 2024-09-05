4c6abc90de8510103601088dc3902c9e4a5b3986
MariaKhlestova <selebina.maria@yandex.ru>
2024-09-06 01:06:28 +0300

    update

--- Lesson6.py
+++ Lesson6.py
@@ -25,3 +25,35 @@ print('Вторая команда:', second_team)
 print('Победители тура:', winners)
 """
 # Задача 3. Двумерный список
+"""
+my_list = [[j_num for j_num in range(i_list, 13, 4)] for i_list in range(1, 5)]
+print(my_list)
+
+# Второй вариант решения
+second_answer = [[value, value + 4, value + 8] for value in range(1, 5)]
+print(second_answer)
+"""
+# задача 4. Список списков
+"""
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+output = [digit
+          for each_list in nice_list
+          for each_list_2 in each_list
+          for digit in each_list_2]
+print(output)
+"""
+# Задача 5. Шифр Цезаря
+"""
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+def caesar_cipher(string, user_shift):
+    char_list = [(alphabet[(alphabet.index(sym) + user_shift) % len(alphabet)] if sym in alphabet else sym) for sym in string]
+    new_str = ''.join(char_list)
+    return new_str
+
+input_str = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+
+output_str = caesar_cipher(input_str, shift)
+print('Зашифрованное сообзение:', output_str)
+"""
