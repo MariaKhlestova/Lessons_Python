f807fd23e68dc33182ee13ef88a9b12823d89e46
MariaKhlestova <selebina.maria@yandex.ru>
2024-09-15 00:27:29 +0300

    update

--- Lesson8.py
+++ Lesson8.py
@@ -25,11 +25,42 @@ sum_file.close()
 # программирования на языке Python. 
 # Напишите программу, которая выводит на экран все строки этого файла в 
 # обратном порядке.
-
+"""
 philosophical_text = open("zen.txt", "r")
 lines = philosophical_text.readlines()
 philosophical_text.close()
 
 for line in reversed(lines):
     print(line.strip())
+"""
 
+# Задание 3. Турнир
+# В файле first_tour.txt записано число K и данные об участниках турнира по 
+# настольной игре «Орлеан»: фамилии, имена и количество баллов, набранных в 
+# первом туре. Во второй тур проходят участники, которые набрали более K баллов в первом туре.
+# Напишите программу, которая выводит в файл second_tour.txt данные всех участников, прошедших во второй тур, с нумерацией.
+
+with open("first_tour.txt", "r") as file:
+    lines = file.readlines()
+    k = int(lines[0])
+    participants = {}
+    filter_player = {}
+    count = 1
+
+for line in lines[1:]:
+    data = line.strip().split()
+    name = f"{data[1][0]}. {data[0]}"
+    score = int(data[-1])
+    participants[name] = score
+    
+for player, score in participants.items():
+    if score > k:
+        filter_player[player] = score
+sorted_filter_player = dict(sorted(filter_player.items(), key=lambda x: x[1], reverse=True))
+
+with open("second_tour.txt", "w") as file:
+    file.write(f"{len(sorted_filter_player)}\n")
+    
+    for player, score in sorted_filter_player.items():
+        file.write(f"{count}) {player} {score}\n")
+        count += 1
