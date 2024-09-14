4f699c0092dcfd92e57efd36b16540bed04def7d
MariaKhlestova <selebina.maria@yandex.ru>
2024-09-15 00:09:18 +0300

    update

--- Lesson8.py
+++ Lesson8.py
@@ -2,7 +2,7 @@
 # Во входном файле numbers.txt записано N целых чисел, которые могут быть 
 # разделены пробелами и концами строк. Напишите программу, которая выводит 
 # сумму чисел во выходной файл answer.txt.
-
+"""
 import os
 
 numbers_file = open("numbers.txt", "r", encoding="utf-8")
@@ -19,3 +19,17 @@ sum_file = open("answer.txt", "w", encoding="utf-8")
 
 sum_file.write(str(total_sum))
 sum_file.close()
+"""
+# Задание 2. Сумма чисел
+# В файле zen.txt хранится так называемый Дзен Пайтона — текст философии 
+# программирования на языке Python. 
+# Напишите программу, которая выводит на экран все строки этого файла в 
+# обратном порядке.
+
+philosophical_text = open("zen.txt", "r")
+lines = philosophical_text.readlines()
+philosophical_text.close()
+
+for line in reversed(lines):
+    print(line.strip())
+
